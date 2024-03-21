1e91d63a42bbd50a502c879133db28377f98e120
nonamebesty <160272703+nonamebesty@users.noreply.github.com>
2024-03-21 08:12:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Jegaa1/sudoa /sudoa
+  git clone https://github.com/nonamebesty/byyyy /byyyy
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /sudoa
+  git clone $UPSTREAM_REPO /byyyy
 fi
-cd /Link-Bypasser-Bot
+cd /byyyy
 pip3 install -U -r requirements.txt
 echo "Starting Bypass Bot...."
 python3 main.py
